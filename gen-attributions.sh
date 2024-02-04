d59c2ea0a3ea5f12162bd375d3a4148de4a1e634
Hess Lewis <hess@ali-lewis.com>
2024-02-04 15:41:11 -0500

    Generate Attributions

--- gen-attributions.sh
+++ gen-attributions.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-MOD_URLS=$(./get-mods.sh)
-for u in $MOD_URLS; do
-	echo $u
+if ! command -v dasel &> /dev/null; then
+	>&2 echo "Please install dasel"
+	exit 1
+fi
+
+get_url() {
+	local mode="$(dasel -f "${1}" -r toml download.mode 2>/dev/null)"
+	local stub=$(basename ${1/.pw.toml/})
+	if [[ "${mode}" == "'metadata:curseforge'" ]]; then
+		URL="https://www.curseforge.com/minecraft/mc-mods/${stub}"
+	else
+		URL="https://modrinth.com/mod/${stub}"
+	fi
+}
+
+for i in mods/*.toml; do
+	NAME=$(dasel -f "${i}" -r toml name)
+	get_url "${i}"
+	echo " - [${NAME}](${URL})"
 done
