74fb03ae20ec3dfe584ded9027f019efcb2b29fb
Hess Lewis <hess@ali-lewis.com>
2024-02-04 15:32:23 -0500

    reform

--- gen-attributions.sh
+++ gen-attributions.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-if ! command -v dasel &> /dev/null; then
-	>&2 echo "Please install dasel"
-	exit 1
-fi
-for i in mods/*.toml; do
-	mode="$(dasel -f "${i}" -r toml download.mode 2>/dev/null)"
-	if [[ "$mode" == "'metadata:curseforge'" ]]; then
-		i=$(basename ${i/.pw.toml/})
-		echo "https://www.curseforge.com/minecraft/mc-mods/${i}"
-	fi
-	
+MOD_URLS=$(./get-mods.sh)
+for u in $MOD_URLS; do
+	echo $u
 done
